535595ea94a51dd3b0ad60796b4f2fbc6169cca2
Thomas Pettinger <pettinger@cqse.eu>
2022-09-07 13:09:36 +0200

    Add todo 2

--- src/main/java/pixelitor/Pixelitor.java
+++ src/main/java/pixelitor/Pixelitor.java
@@ -59,6 +59,7 @@ public class Pixelitor {
 
     public static void main(String[] args) {
         // New TODO
+        // New TODO 2
         // the app can be put into development mode by
         // adding -Dpixelitor.development=true to the command line
         if ("true".equals(System.getProperty("pixelitor.development"))) {
